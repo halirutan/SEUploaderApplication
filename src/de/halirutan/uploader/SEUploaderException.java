ab4ca2b9b5efee5f9b83b7310a23dc7780e3dc99
halirutan <pscheibe@trm.uni-leipzig.de>
2013-06-28 02:41:51 +0200

    Initial commit

---
+++ src/de/halirutan/uploader/SEUploaderException.java
@@ -0,0 +1,10 @@
+package de.halirutan.uploader;
+
+/**
+ * @author patrick (6/28/13)
+ */
+public class SEUploaderException extends Throwable {
+  public SEUploaderException(String message) {
+    super(message);
+  }
+}
