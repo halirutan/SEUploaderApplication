2aedb186b5f7b9a798de6cb047d9140367f75ae2
halirutan <pscheibe@trm.uni-leipzig.de>
2013-11-25 06:24:01 +0100

    Fixed uploader after stackexchange API change. The core problem was, that SE returns the upload URL embedded in a html page.

--- SEUploader/Installer.m
+++ SEUploader/Installer.m
@@ -3,30 +3,32 @@
 (* :Date: 6/28/13 *)
 
 If[$VersionNumber < 9,
-    Print["This works only in Mathematica version 9"],
+  Print[ "This works only in Mathematica version 9" ],
 
-    Block[{url,
-        zipFile = FileNameJoin[{$TemporaryDirectory, "master.zip"}],
-        extractedDir = FileNameJoin[{$TemporaryDirectory, "SEUploaderApplication-master"}]},
+  Block[{url,
+    zipFile = FileNameJoin[{$TemporaryDirectory, "master.zip" }],
+    extractedDir = FileNameJoin[{$TemporaryDirectory, "SEUploaderApplication-master" }]},
 
-        url = "https://github.com/halirutan/SEUploaderApplication/archive/master.zip";
-        If[FileExistsQ[zipFile] || DirectoryQ[extractedDir],
-            Print["Error, one of the following already exists. Please remove these files/directories and start again:"];
-            Print[zipFile];
-            Print[extractedDir];
-            Abort[];
-        ];
+    url = "https://github.com/halirutan/SEUploaderApplication/archive/master.zip";
+    If[FileExistsQ[zipFile] || DirectoryQ[extractedDir] || DirectoryQ[FileNameJoin[{$UserAddOnsDirectory, "Applications" , "SEUploader" }]],
+      Print[ "Error, the following file/directory already exists. Please remove it and restart:" ];
+      If[FileExistsQ[zipFile], Print[zipFile]];
+      If[DirectoryQ[extractedDir], Print[extractedDir]];
+      If[DirectoryQ[FileNameJoin[{$UserAddOnsDirectory, "Applications" , "SEUploader" }]],
+        Print[FileNameJoin[{$UserAddOnsDirectory, "Applications" , "SEUploader" }]]];
+      Abort[];
+    ];
 
-        file = URLSave[url, zipFile];
-        If[Not[FileExistsQ[file]],
-            Print["Couldn't download resource"];
-            Abort[];
-        ];
-        ExtractArchive[file, $TemporaryDirectory];
-        CopyDirectory[FileNameJoin[{$TemporaryDirectory, "SEUploaderApplication-master", "SEUploader"}],
-            FileNameJoin[{$UserAddOnsDirectory, "Applications", "SEUploader"}]];
-        DeleteDirectory[FileNameJoin[{$TemporaryDirectory, "SEUploaderApplication-master"}], DeleteContents -> True];
-        DeleteFile[file];
-        Print["Please restart Mathematica to see the palette in the Palette menu"]
-    ]
+    file = URLSave[url, zipFile];
+    If[Not[FileExistsQ[file]],
+      Print[ "Couldn't download resource" ];
+      Abort[];
+    ];
+    ExtractArchive[file, $TemporaryDirectory];
+    CopyDirectory[FileNameJoin[{$TemporaryDirectory, "SEUploaderApplication-master" , "SEUploader" }],
+      FileNameJoin[{$UserAddOnsDirectory, "Applications" , "SEUploader" }]];
+    DeleteDirectory[FileNameJoin[{$TemporaryDirectory, "SEUploaderApplication-master" }], DeleteContents -> True];
+    DeleteFile[file];
+    Print[ "Please restart Mathematica to see the palette in the Palette menu" ]
+  ]
 ]
--- src/de/halirutan/uploader/SEUploaderException.java
+++ src/de/halirutan/uploader/SEUploaderException.java
@@ -3,7 +3,7 @@ package de.halirutan.uploader;
 /**
  * @author patrick (6/28/13)
  */
-public class SEUploaderException extends Throwable {
+public class SEUploaderException extends Exception {
   public SEUploaderException(String message) {
     super(message);
   }
